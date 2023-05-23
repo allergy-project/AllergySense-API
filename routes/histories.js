fffc7aa1c44b38d950900478b3efeb28b5eab064
Gre-ys <rdsuryamp@gmail.com>
2023-05-23 14:00:38 +0700

    Delete Cache Config

--- routes/histories.js
+++ routes/histories.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const historiesController = require("../controllers/historiesController");
 const validateAuth = require("../middlewares/validateAuthMiddleware");
 const historyValidation = require("../middlewares/historyValidationMiddleware")
-const apicache = require("apicache");
-const cache = apicache.middleware;
+
+// For Cache Content
+// const apicache = require("apicache");
+// const cache = apicache.middleware;
 
 // Get All Histories
-router.get("/", [validateAuth, cache("30 minutes")], historiesController.getHistories);
+// router.get("/", [validateAuth, cache("30 minutes")], historiesController.getHistories); // Cache Version
+router.get("/", validateAuth, historiesController.getHistories);
 
 // Get Hisotry By Id
 router.get("/:id", validateAuth, historiesController.getHistoryById);
