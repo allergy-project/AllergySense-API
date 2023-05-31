04e1cf858fa2dbd9ba14733139625fec2b11f658
Gre-ys <rdsuryamp@gmail.com>
2023-05-31 23:05:39 +0700

    test allergy detection

--- middlewares/mlValidationMiddleware.js
+++ middlewares/mlValidationMiddleware.js
@@ -14,9 +14,12 @@ exports.allergyDetection = () => async (req, res, next) => {
             if(data[requiredData].length == 0) return res.status(400).json({status_code:400, message: (req.isIndo)? `${requiredData} Wajib Diisi!` : `${requiredData} is Required!`});
             
             // Convert to String if Problem and Trim Data
-            if (requiredData != "allergen_number") data[requiredData] = data[requiredData].toString().trim();
+            if (requiredData != "allergen_code_number") data[requiredData] = data[requiredData].toString().trim();
         }
             
+        // Check allergen_code_number must in range 0 - 161
+        if (!(data["allergen_code_number"] >= 0 && data["allergen_code_number"] <= 161)) return res.status(400).json({ status_code:400, message: (req.isIndo)? `Masukan Allergen/Bahan Makanan Sesuai Jangkauan!` : "Input Allergen/Ingredient According Given Scope!" });
+        
         // Wrap Data for Used in Controller
         req.data = data;
         
