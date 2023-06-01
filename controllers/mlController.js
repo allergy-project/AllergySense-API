29d60f73f670a58a6a8ca23518de0c5af2913bb8
Gre-ys <rdsuryamp@gmail.com>
2023-06-01 13:29:10 +0700

    Test allergy detection

--- controllers/mlController.js
+++ controllers/mlController.js
@@ -1,8 +1,15 @@
 const tf = require('@tensorflow/tfjs-node');
 const path = require("path");
 const fs = require("fs");
+const History = require("../models/History")
 
 exports.allergyDetection = async (req, res) => {
+    // Cek User Login
+    if (!req.user || req.user == null || req.user.length == 0) return res.status(401).json({ status_code:401, message: (req.isIndo)? `Pengguna Belum Terautentikasi!` : "User Not Authenticated!" });
+
+    // Get User Id from Middleware
+    const user_id = req.user.id;
+    
     // Check File Image Exist
     if(!req.file) return res.status(400).json({status_code:400, message: (req.isIndo)? "Upload File Gambar!" : "No Image File Provided!"});
     
@@ -10,6 +17,7 @@ exports.allergyDetection = async (req, res) => {
         const file = req.file;
         // req.data
         
+        // Allergy Detection Logic(Determine Allergy or Not)
         // Load Model
         modelLocation = path.join(__dirname, "../utils/allergyDetection.json");;
         const model = await tf.loadLayersModel(`file://${modelLocation}`);
@@ -27,14 +35,34 @@ exports.allergyDetection = async (req, res) => {
         const predictions = model.predict(inputTensor);
         const result = predictions.arraySync();
         
+        // Check Allergy or Not
+        let isAllergy = null
         let message = ""
         if (result[0][0] > result[0][1]){
-            message = (req.isIndo)? "Terindikasi Alergi!" : "Indicated Allergy!";
+            isAllergy = true;
+            message = (req.isIndo)? "Terindikasi Alergi" : "Indicated Allergy";
         }else if (result[0][1] > result[0][0]){
-            message = (req.isIndo)? "Tidak Terindikasi Alergi!" : "Not Indicated Allergy!";
+            isAllergy = false
+            message = (req.isIndo)? "Tidak Terindikasi Alergi" : "Not Indicated Allergy";
         }
         
-        res.status(200).json({status_code:200, message, result[0], data: req.data});
+        // Allergy Classification Logic(Determine Type of Allergy) allergy, suggest, suggest_indo, is_allergy, created_at, image_url
+        // Model Usage Goes Here
+        const data = {allergy: `Test Allergy-${req.data.allergen_code_number}`, suggest: `Dont eat related to ${req.data.allergen_code_number}`, suggest_indo: `Jangan makan yang berkaitan dengan ${req.data.allergen_code_number}`}
+        
+        // Prepare Data for Add to Histories
+        // Data for Created At(Unixtime)
+        const created_at = Date.now(); 
+        
+        // Clear Allergy Field if Not Allergy
+        if (!isAllergy) data.allergy = "Not Allergy";
+        
+        // Add History
+        const historyDoc = await History.add({ ...data, created_at, user_id: req.user.id, image_url: req.image_url, problem: req.data.problem, is_allergy: isAllergy });
+        
+        if (isAllergy) message = message+` ${data.allergy}`;
+        
+        res.status(200).json({status_code:200, message, data: {history_id: historyDoc.id}});
         
     }catch(error){
         console.log(error);
